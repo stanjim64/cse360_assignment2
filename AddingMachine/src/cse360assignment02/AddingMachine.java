64214b9bde1dbfdbc989c4254c1dbb6d4b8ef5f0
tanjim <tanjim@localhost>
2020-10-05 22:43:21 -0700

    the edited file after changes

--- AddingMachine/src/cse360assignment02/AddingMachine.java
+++ AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,67 @@
 package cse360assignment02;
 
-public class AddingMachine {
-  private int total;
+public class AddingMachine 
+{
+	private int total;
+	private String history; 
+	
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+		history = "0";
+	}
   
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
+	public int getTotal () 
+	{
+		return total;
+	}
   
-  public int getTotal () {
-    return 0;
-  }
-  
-  public void add (int value) {
-  }
+	public void add (int value) 
+	{
+		int temp1 = value;
+		total = total + temp1;
+		history = history + " + " + temp1;  
+	}
 
-  public void subtract (int value) {
-  }
+	public void subtract (int value) 
+	{
+		int temp2 = value;
+		total = total + temp2;
+		history = history + " - " + temp2;
+	}
 
-  public String toString () {
-    return "";
-  }
+	public String toString () 
+	{
+		return history;
+	}
 
-  public void clear() {
-  }
+	public void clear() 
+	{
+		history = "";
+	}
+	
+	
+	
+	// the main method
+	
+	public static void main(String[] args)
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		
+		myCalculator.add (4); 
+		myCalculator.subtract (2); 
+		myCalculator.add(5);
+		
+		System.out.println(myCalculator.toString());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 } 
+
+
--- AddingMachine/src/thing1.java
+++ AddingMachine/src/thing1.java
@@ -3,7 +3,7 @@ public class thing1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("its compiles ");
+		System.out.println("its compiles");
 		
 		System.out.println("secong edit");
 	}
